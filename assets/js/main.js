26b276aaae478492e40f2a1775fa94ce9eaaa5e5
anurag-bhattacharjee <81433930+anurag-bhattacharjee@users.noreply.github.com>
2021-11-23 14:22:49 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,6 +43,7 @@ let header = $(`
         <li><a class="white-text" href="projects.html">Projects</a></li>
         <li><a class="white-text" href="research.html">Research</a></li>
         <li><a class="white-text" href="education.html">Education</a></li>
+        <li><a class="white-text" href="resume.html">My Resume</a></li>
         <li><a class="white-text" href="skill.html">Skills</a></li>
         <li><a class="white-text" href="index.html">Blog</a></li>
         <!--TOGGLE FOR LIGHT/DARK MODE-->
