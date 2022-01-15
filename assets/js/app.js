92410ef48fc9c53ea5805bf19f1e8f4b93042dad
anurag-bhattacharjee <81433930+anurag-bhattacharjee@users.noreply.github.com>
2022-01-15 14:22:37 -0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -164,7 +164,7 @@ let footer = $(`
                                    c117.9-117.9,82.9-167.7,82.9-167.7l-2.8-0.1C241.6,182.3,238.2,181.9,233.2,181.5z" />
             </svg>
             </a>
-           <a class="social-button linkedin" href="https://www.linkedin.com/in/anuragbhattacharjee6243/" target="_blank" >
+           <a class="social-button linkedin" href="https://www.linkedin.com/in/anuragbhatta" target="_blank" >
            <!-- svg code for linkedin icon -->
               <svg class="linkedin-icon-footer" xmlns="http://www.w3.org/2000/svg" width="35" viewBox="0 0 24 24" fill="#0e76a8"
                 stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin">
@@ -183,7 +183,7 @@ let footer = $(`
                  <path id="camera" class="st0" d="M79,60.5h44c10.5,0,19,8.5,19,19v44c0,10.5-8.5,19-19,19H79c-10.5,0-19-8.5-19-19v-44                                                                            C60,69,68.5,60.5,79,60.5z" />
               </svg>
             </a>      
-            <a class="social-button github" href="https://github.com/Toni8215" target="_blank">
+            <a class="social-button github" href="https://github.com/anurag-bhattacharjee" target="_blank">
             <!-- SVG code for Github icon -->
             <svg class="github-icon-footer" width="45px" height="45px" viewBox="0 0 300 300">
                 <!-- body -->
